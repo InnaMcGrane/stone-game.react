e14b31049c6588e849504f5b60837bcbd12bc346
InnaMcGrane <innamcgrane@gmail.com>
2024-10-16 07:40:51 +0100

    create button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,8 @@
-function Button(){
-    return (
-    <button className="btn btn--start">Start Game</button>
-    );
+import cn from "classnames";
+import styles from './button.module.css'
+
+function Button({type, text, disabled, handler}){
+    return <button className={cn(styles["btn"], styles[`btn--${type}`])} disabled={disabled} onClick={() => handler()}>{text}</button>
 }
+
 export default Button;
