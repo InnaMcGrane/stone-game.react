895f3292630911ba89c77ed5e79c264bbcb4c19a
InnaMcGrane <innamcgrane@gmail.com>
2024-10-19 08:35:44 +0100

    create data start

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,12 +1,23 @@
 import cn from "classnames";
 import styles from "./game.module.css"
+import Button from "../Button/Button";
+import data from "../../data";
+import Stone from "../Stone/Stone";
 
 function Game () {
     return <div className={cn(styles["game"])}>
-        <div className={cn(styles["game__btn"])}></div>
+        <div className={cn(styles["game__btn"])}> 
+            <Button type="start" text="Start Game" handler={() => {}}/>
+        </div>
         <span className={cn(styles["game__found"])}>found parts: 0</span>
         <span className={cn(styles["game__total"])}>total parts: 0</span>
-        <div className={cn(styles["game__wrapper"])}></div>
+        <div className={cn(styles["game__wrapper"])}>
+          {
+            data.map((el) => {
+                return <Stone id={el.id} hide={false} disabled={true} img={el.img} key={el.id} />
+            })
+          }
+        </div>
         </div>;
   }
 
--- src/components/Stone/Stone.jsx
+++ src/components/Stone/Stone.jsx
@@ -1,9 +1,12 @@
-function Stone({hide, disabled, img}) {
-    return (
-      <button className={`stone ${hide ? "stone--hide" : ""}`} disabled={disabled}>
-        <img className="stone__img" src={`./image/${img}.png`} alt="" />
-      </button>
-    );
+import cn from "classnames";
+import styles from "./stone.module.css";
+
+function Stone({ id, hide, disabled, img }) {
+  return (
+    <button className={cn(styles["stone"], hide ? styles["stone-hide"] : "")} disabled={disabled}>
+      <img className={cn(styles["stone__img"])} src={img} alt="" />
+    </button>
+  );
 }
 
 export default Stone;
